1f43a603f0caa837a9456d15022c507b5f01671d
Mogu programming <kbu94984@gmail.com>
2015-04-12 13:38:36 +0900

    add readme.md

---
+++ hello.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int c;
+    std::cout <<"hello world."<< std::endl;
+    cin>>c;
+    std::cout<<c<<std::endl;
+    for(int i=0;i<10;i++){
+        for(int j=0;j<10;j++){
+        std::cout <<"hello world."<<std::endl;
+        }
+        std::cout<<j<<std::endl;
+    }
+
+    return 0;
+}
