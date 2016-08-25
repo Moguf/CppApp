48924cc2320d0824d8b4fd5b1ea40d092f8de8f2
Mogu programming <kbu94984@gmail.com>
2016-08-25 13:41:54 +0900

    MOD: ex1.cpp.

--- src2/ch02/src/ex1.cpp
+++ src2/ch02/src/ex1.cpp
@@ -2,6 +2,7 @@
 #include <string>
 
 
+enum class Color {red, blue, green};
 int main()
 {
     int v[] = {10,11,23,10000};
@@ -16,4 +17,40 @@ int main()
 
     for(auto& x: v)
         std::cout << &x << std::endl;
+    
+    Color col = Color::red;
+    Color col2 = Color::blue;
+    if( col != col2 ){
+        std::cout << "red != blue" << std::endl;
+    }
+        
 }
+
+
+class Vector{
+public:
+    Vector(int s);
+    double& operator[](int i) { return elem[i];}
+    int size(){ return sz;}
+private:
+    double *elem;
+    int sz;
+};
+
+Vector::Vector(int s):elem{new double[s]},sz{s}
+{
+}
+
+
+enum class Traffic_light{ yellow, red, green};
+
+Traffic_light& operator++(Traffic_light& t)
+{
+    switch(t){
+    case Traffic_light::green: return t=Traffic_light::yellow;
+    case Traffic_light::yellow: return t=Traffic_light::red;
+    case Traffic_light::red: return t=Traffic_light::green;        
+    }
+}
+
+    
